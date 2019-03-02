fc6602923a51158aa9410f735750a3de2694be09
Minrui Zhang <zmr227@gmail.com>
2019-03-02 10:42:41 -0500

    Add: IP Lab-7
    MVC

---
+++ Lab/Lab-7/Lab7/Lab7/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        // 
+        // GET: /Home/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /Home/About/
+        public IActionResult About(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+        //
+        // GET: /Home/Labs/
+        public IActionResult Labs()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Lab/Lab-7/Lab7/Lab7/Models/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Lab7.Models
+{
+    public class Project
+    {
+        public int Id { get; set; } = -1;
+        public string Name { get; set; }
+        public string Language { get; set; }
+        public string Technology { get; set; }
+        public string Purpose { get; set; }
+        public string Notes { get; set; }
+
+        public Project()
+        {
+        }
+    }
+}
