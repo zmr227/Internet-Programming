3d3e19827410c7dbd5a0f8f71a0289859d370666
zmr227 <36898724+zmr227@users.noreply.github.com>
2019-04-10 06:02:52 +0800

    Implement Webservice Api for IP Lab12
    Define an Api for uploading, downloading, deleting, and modifying JSON meta data about files

---
+++ Lab/Lab-12/Lab12/Lab12/Program.cs
@@ -0,0 +1,30 @@
+﻿/////////////////////////////////////////////////////////////
+// Program.cs - Entry Point for Web Api                    //
+//                                                         //
+// Jim Fawcett, CSE686 - Internet Programming, Spring 2019 //
+/////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Lab12
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      CreateWebHostBuilder(args).Build().Run();
+    }
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>();
+  }
+}
